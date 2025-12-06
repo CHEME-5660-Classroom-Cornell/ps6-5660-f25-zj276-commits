cbf83f643588d59cbc1490322f337d42ca24a23a
zj276-commits <zj276@cornell.edu>
2025-12-06 15:58:15 -0500

    Update Include.jl

--- Include.jl
+++ Include.jl
@@ -7,9 +7,28 @@ const _PATH_TO_SIMS = joinpath(_ROOT, "sims");
 
 # make sure all is up to date -
 using Pkg
-if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
-    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
-    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+
+# Activate the project environment
+Pkg.activate(".")
+
+# Install standard packages if needed
+_pkgs = ["DataFrames", "Statistics", "StatsBase", "Plots", "Colors", "PrettyTables", 
+         "CSV", "Distributions", "FileIO", "JLD2", "HypothesisTests", "StatsPlots", 
+         "LinearAlgebra", "ProgressMeter", "Test"]
+
+for pkg in _pkgs
+    try
+        Pkg.add(pkg)
+    catch
+        println("Warning: Could not add $pkg")
+    end
+end
+
+# Try to add the custom VL package (non-critical if it fails)
+try
+    Pkg.add(url="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+catch e
+    println("Warning: Could not add VLQuantitativeFinancePackage: $e")
 end
 
 # load external packages -
